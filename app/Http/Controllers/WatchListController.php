008d2ed2cd43d719834f68c805eb67ef935b8cd8
Hakan ERSU <hakanersu@gmail.com>
2020-06-19 13:03:42 +0300

    watch list feature, migration and models

---
+++ app/Http/Controllers/WatchListController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WatchList;
+use Illuminate\Http\Request;
+
+class WatchListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WatchList  $watchList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WatchList $watchList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WatchList  $watchList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WatchList $watchList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WatchList  $watchList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WatchList $watchList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WatchList  $watchList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WatchList $watchList)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_06_19_095538_create_watch_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watch_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('tconst')->index();
+            $table->foreignId('user_id')->index();
+            $table->string('title_type');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watch_lists');
+    }
+}
