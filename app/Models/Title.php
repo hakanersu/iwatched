d889a45fcf933bf7092e331b82617f2d218cd7ea
Hakan ERSU <hakanersu@gmail.com>
2022-02-26 23:52:27 +0300

    update

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -87,7 +87,7 @@ class Title extends Model
 
     public static function tmdb($id, $type='movie_results'): string
     {
-        $token = auth()->user()->token ?? config("watched.tmdb_key");
+        $token = (auth()->check() && auth()->user()->token) ?? config("watched.tmdb_key");
 
         $response = Http::get("https://api.themoviedb.org/3/find/{$id}?api_key=".$token."&external_source=imdb_id");
 
@@ -95,7 +95,7 @@ class Title extends Model
 
         $url = "https://image.tmdb.org/t/p/w500{$posterPath}";
 
-        if ($posterPath && auth()->user()->save_posters) {
+        if ($posterPath && (auth()->check() || auth()->user()->save_posters)) {
             FetchPoster::dispatch($url, $id);
         }
 
