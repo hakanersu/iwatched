6aa6c00bdf4a8287ab29eaaecf258002944d1b1a
Hakan ERSU <hakanersu@gmail.com>
2022-02-26 23:45:21 +0300

    Home page update

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
         'titles' => cache()->rememberForever('titles', function () {
             return number_format(\App\Models\Title::count());
         }),
